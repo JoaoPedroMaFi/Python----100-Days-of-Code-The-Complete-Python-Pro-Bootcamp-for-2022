ab4520753469e3815c45a928ff33c9e022d95371
Pedro <joao.pedro.f93@gmail.com>
2022-11-22 01:43:36 +0000

    Day 37 - API Practice - Http Verbs

---
+++ Day 37/main.py
@@ -0,0 +1,60 @@
+import requests
+from datetime import datetime
+
+USERNAME = "NONE"
+TOKEN= "NONE"
+GRAPH_ID = "NONE"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id":GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "sora"
+
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_endpoint =f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+today = datetime.now()
+pixel_config = {
+    "date": today.strftime('%Y%m%d'),
+    "quantity": "11"
+
+}
+# response = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixel_endpoint}/{today.strftime('%Y%m%d')}"
+
+pixel_update_config = {
+    "quantity": "20"
+}
+# response = requests.put(url=update_endpoint, json=pixel_update_config, headers=headers)
+# print(response.text)
+
+
+delete_endpoint = f"{pixel_endpoint}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=update_endpoint, headers=headers)
+print(response.text)
